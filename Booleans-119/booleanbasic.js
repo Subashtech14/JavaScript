d99babbd8034995876435bcd675a66ebbeeca09f
Subash <Subashtech14@gmail.com>
2023-06-19 10:59:34 +0530

    Boolean basic

---
+++ Booleans-119/booleanbasic.js
@@ -0,0 +1,7 @@
+// || they are called value-preservation
+console.log(12||"hello")
+console.log(0||"hello")
+// && Short circuting Operator
+const x= false && console.log('hello');
+console.log(x);
+console.log(!true);
