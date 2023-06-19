f06fdb8dd0cf9ce43d139ed51e868211f97ff1a2
Subash <Subashtech14@gmail.com>
2023-06-19 20:34:50 +0530

    Math Module

---
+++ MathModule/MathExample.js
@@ -0,0 +1,19 @@
+//Cube Root
+console.log(Math.cbrt(25));
+console.log(Math.exp(0));
+//Returns the natural logariyhm of x
+//Inverse of Natural exp()
+console.log(Math.log(1));
+console.log(Math.log10(100));
+console.log(Math.log2(100));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(25));
+//Rounding
+console.log(Math.ceil(2.1));
+//Returns the closest to the Integer
+console.log(Math.floor(2.9));
+//Rounding
+console.log(Math.round(2.5));
+//Removes the decimal Part
+console.log(Math.trunc(2.1));
+
---
+++ MathModule/trifnomentric.js
@@ -0,0 +1,12 @@
+//Return the inverse of cosine function
+console.log(Math.acos(0));
+//Returns the inverse hyberbolic cosine function
+console.log(Math.acosh(0));
+//Returns the inverse of sine
+console.log(Math.asin(0));
+//Returns the inverse hyperbolic sine
+consolelog(Math.asinh(0));
+//Returns the inverse tangent
+console.log(Math.atan(0));
+//Returns the arc tangent
+console.log(Math.atan2(0,0));
